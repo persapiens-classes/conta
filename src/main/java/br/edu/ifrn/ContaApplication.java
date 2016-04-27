67005cf1dd745914871ec83176d05b7f9271e16b
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-04-27 17:51:23 -0300

    fix #1

---
+++ src/main/java/br/edu/ifrn/ContaApplication.java
@@ -0,0 +1,14 @@
+package br.edu.ifrn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ContaApplication {
+    
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()                
+                .sources(ContaApplication.class)
+                .run(args);
+    }
+}
