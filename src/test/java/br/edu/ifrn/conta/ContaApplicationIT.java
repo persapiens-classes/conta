f15f317d8e70b7c03d9ddf443569ca9ce7dd5e08
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-05-04 20:56:17 -0300

    fix #21

--- src/test/java/br/edu/ifrn/conta/ContaApplicationIT.java
+++ src/test/java/br/edu/ifrn/conta/ContaApplicationIT.java
@@ -1,15 +1,13 @@
 package br.edu.ifrn.conta;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ContaApplication.class)
 @WebAppConfiguration
-public class ContaApplicationTests {
+public class ContaApplicationIT extends AbstractTestNGSpringContextTests {
 
     @Test
     public void contextLoads() {
