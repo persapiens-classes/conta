86856764da2d54d23b2b66e1cde456e794aa88f5
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-21 10:04:37 -0300

    fix #62 regra de negócio verificarAtributos no save de LancamentoServico verificada

--- src/main/java/br/edu/ifrn/conta/servico/LancamentoServico.java
+++ src/main/java/br/edu/ifrn/conta/servico/LancamentoServico.java
@@ -1,10 +1,13 @@
 package br.edu.ifrn.conta.servico;
 
+import br.edu.ifrn.conta.dominio.ContaDebito;
+import br.edu.ifrn.conta.dominio.ContaCredito;
 import br.edu.ifrn.conta.dominio.ContaPatrimonio;
 import br.edu.ifrn.conta.dominio.Dono;
 import br.edu.ifrn.conta.dominio.Lancamento;
 import br.edu.ifrn.conta.persistencia.LancamentoRepository;
 import java.math.BigDecimal;
+import java.util.Date;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -21,4 +24,43 @@ public class LancamentoServico extends AbstratoServico<Lancamento, Long> {
     public BigDecimal saldo(Dono dono, ContaPatrimonio contaPatrimonio) {
         return lancamentoRepository.saldo(dono, contaPatrimonio);
     }    
+
+    @Override
+    public void save(Lancamento objeto) {
+        objeto.verificarAtributos();
+        
+        super.save(objeto);
+    }
+    
+    public void transferir(BigDecimal valor
+        , Dono donoDebito, ContaDebito contaDebito, ContaPatrimonio contaPatrimonioADebitar
+        , Dono donoCredito, ContaPatrimonio contaPatrimonioACreditar, ContaCredito contaCredito) {
+        
+        if (donoCredito.equals(donoDebito)) {
+            throw new IllegalArgumentException("Donos das contas devem ser diferentes: "
+                + donoDebito + " = " + donoCredito);
+        }
+        
+        Date data = new Date();
+        
+        Lancamento lancamentoComDespesa = Lancamento.builder()
+            .contaEntrada(contaDebito)
+            .contaSaida(contaPatrimonioADebitar)
+            .dono(donoDebito)
+            .valor(valor)
+            .data(data)
+            .descricao("Lançamento de débito de uma transferência")
+            .build();
+        save(lancamentoComDespesa);
+        
+        Lancamento lancamentoComReceita = Lancamento.builder()
+            .contaEntrada(contaPatrimonioACreditar)
+            .contaSaida(contaCredito)
+            .dono(donoCredito)
+            .valor(valor)
+            .data(data)
+            .descricao("Lançamento de crédito de uma transferência")
+            .build();
+        save(lancamentoComReceita);    
+    }
 }
