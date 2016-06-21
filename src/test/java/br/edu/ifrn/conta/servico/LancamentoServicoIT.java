fbf4be8dbfd8f61e4960e0e1f366c1753fd01bbb
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-21 10:09:50 -0300

    fix #64 testes de integração das regra de negócio de Lancamento adicionados

--- src/test/java/br/edu/ifrn/conta/servico/LancamentoServicoIT.java
+++ src/test/java/br/edu/ifrn/conta/servico/LancamentoServicoIT.java
@@ -5,8 +5,15 @@ import br.edu.ifrn.conta.dominio.ContaCredito;
 import br.edu.ifrn.conta.dominio.ContaDebito;
 import br.edu.ifrn.conta.dominio.ContaPatrimonio;
 import br.edu.ifrn.conta.dominio.Dono;
-import br.edu.ifrn.conta.persistencia.FabricaDominio;
+import br.edu.ifrn.conta.dominio.Lancamento;
+import br.edu.ifrn.conta.persistencia.ContaCreditoFabrica;
+import br.edu.ifrn.conta.persistencia.ContaDebitoFabrica;
+import br.edu.ifrn.conta.persistencia.ContaPatrimonioFabrica;
+import br.edu.ifrn.conta.persistencia.DonoFabrica;
+import br.edu.ifrn.conta.persistencia.LancamentoFabrica;
+import br.edu.ifrn.conta.persistencia.ValorInicialDoDonoNaContaPatrimonioFabrica;
 import java.math.BigDecimal;
+import java.util.Date;
 import javax.inject.Inject;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -21,10 +28,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LancamentoServicoIT extends AbstractTestNGSpringContextTests {
     
     @Inject
-    private FabricaDominio dominioFactory;
+    private LancamentoFabrica lancamentoFabrica;
+    
+    @Inject
+    private DonoFabrica donoFabrica;
     
     @Inject
     private LancamentoServico lancamentoServico;
+    
+    @Inject
+    private ValorInicialDoDonoNaContaPatrimonioFabrica valorInicialDoDonoNaContaPatrimonioFabrica;
+    
+    @Inject
+    private ContaPatrimonioFabrica contaPatrimonioFabrica;
+    
+    @Inject
+    private ContaCreditoFabrica contaCreditoFabrica;
+    
+    @Inject
+    private ContaDebitoFabrica contaDebitoFabrica;
     
     @BeforeMethod
     void deletarTodos()
@@ -39,26 +61,58 @@ public class LancamentoServicoIT extends AbstractTestNGSpringContextTests {
     
     public void saldo800() {
         // cria o ambiente de teste
-        Dono papai = dominioFactory.papai();
+        Dono papai = donoFabrica.papai();
 
         ContaPatrimonio poupanca = 
-            dominioFactory.poupanca();
+            contaPatrimonioFabrica.poupanca();
 
         ContaDebito gasolina = 
-            dominioFactory.gasolina();
+            contaDebitoFabrica.gasolina();
 
         ContaCredito bolsa = 
-            dominioFactory.estagio();
+            contaCreditoFabrica.estagio();
         
-        dominioFactory.valorInicialDoDonoNaContaPatrimonio(
+        valorInicialDoDonoNaContaPatrimonioFabrica.valorInicialDoDonoNaContaPatrimonio(
             papai, poupanca, new BigDecimal(1000));
         
-        dominioFactory.lancamento(papai, gasolina, poupanca, new BigDecimal(500));
-        dominioFactory.lancamento(papai, poupanca, bolsa, new BigDecimal(300));
+        lancamentoFabrica.lancamento(papai, gasolina, poupanca, new BigDecimal(500));
+        lancamentoFabrica.lancamento(papai, poupanca, bolsa, new BigDecimal(300));
         
         // executa a operacao a ser testada        
         // verifica o efeito da execucao da operacao a ser testada
         assertThat(lancamentoServico.saldo(papai, poupanca))
             .isEqualTo(new BigDecimal(800).setScale(2));
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void lancamentoComContaEntradaInvalida() {
+        Lancamento lancamento = Lancamento.builder()
+            .contaEntrada(contaCreditoFabrica.estagio())
+            .contaSaida(contaPatrimonioFabrica.poupanca())
+            .valor(BigDecimal.TEN)
+            .data(new Date())
+            .dono(donoFabrica.papai())
+            .build();
+        
+        lancamentoServico.save(lancamento);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void lancamentoComContaSaidaInvalida() {
+        Lancamento lancamento = Lancamento.builder()
+            .contaEntrada(contaPatrimonioFabrica.poupanca())
+            .contaSaida(contaDebitoFabrica.gasolina())
+            .valor(BigDecimal.TEN)
+            .data(new Date())
+            .dono(donoFabrica.papai())
+            .build();
+        
+        lancamentoServico.save(lancamento);
+    }
+    
+    public void transferenciaDePapaiParaMamae() {
+        lancamentoServico.transferir(BigDecimal.TEN
+            , donoFabrica.papai(), contaDebitoFabrica.despesaComConjuge(), contaPatrimonioFabrica.poupanca()
+            , donoFabrica.mamae(), contaPatrimonioFabrica.poupanca(), contaCreditoFabrica.receitaComConjuge());
     }
 }
