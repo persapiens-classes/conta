e390e786c2e1bf5c2b89dc952614ab3e3499375b
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-10-12 08:10:07 -0300

    corrigindo checkstyle

--- src/main/java/br/edu/ifrn/conta/ContaApplication.java
+++ src/main/java/br/edu/ifrn/conta/ContaApplication.java
@@ -27,5 +27,5 @@ public class ContaApplication {
 
 	protected ContaApplication() {
 	}
-		
+
 }
--- src/main/java/br/edu/ifrn/conta/ContaApplicationMain.java
+++ src/main/java/br/edu/ifrn/conta/ContaApplicationMain.java
@@ -32,5 +32,5 @@ public class ContaApplicationMain {
 			.sources(ContaApplication.class)
 			.run(args);
 	}
-		
+
 }
