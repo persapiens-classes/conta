56bb1c44c92adf32e4d51ea89c55b86550aa5f01
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-04-03 09:49:47 -0300

    refactor system layout using primefaces saga theme.

--- src/main/java/br/edu/ifrn/conta/visao/RedirectToIndexController.java
+++ src/main/java/br/edu/ifrn/conta/visao/RedirectToIndexController.java
@@ -29,6 +29,6 @@ public class RedirectToIndexController  {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String redirect() {
-		return "redirect:index.faces";
+		return "redirect:dono.faces";
 	}
 }
