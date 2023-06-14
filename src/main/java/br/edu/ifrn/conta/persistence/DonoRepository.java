ecfc8570dc92e20ec6f8ab42bcef52b13767d8ac
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-06-13 23:27:01 -0300

    fix endpoint path

--- src/main/java/br/edu/ifrn/conta/persistence/DonoRepository.java
+++ src/main/java/br/edu/ifrn/conta/persistence/DonoRepository.java
@@ -3,11 +3,13 @@ package br.edu.ifrn.conta.persistence;
 import br.edu.ifrn.conta.domain.Dono;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * CrudRepository with method definitions.
  */
+@RepositoryRestResource(collectionResourceRel = "donos", path = "donos")
 public interface DonoRepository extends CrudRepository<Dono, Long> {
 
 	Dono findByDescricao(String descricao);
--- src/main/java/br/edu/ifrn/conta/persistence/ValorInicialDoDonoNaContaPatrimonioRepository.java
+++ src/main/java/br/edu/ifrn/conta/persistence/ValorInicialDoDonoNaContaPatrimonioRepository.java
@@ -5,10 +5,12 @@ import br.edu.ifrn.conta.domain.Dono;
 import br.edu.ifrn.conta.domain.ValorInicialDoDonoNaContaPatrimonio;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * CrudRepository with method definitions.
  */
+@RepositoryRestResource(collectionResourceRel = "valoresIniciaisDoDonoNaContaPatrimonio", path = "valoresIniciaisDoDonoNaContaPatrimonio")
 public interface ValorInicialDoDonoNaContaPatrimonioRepository extends CrudRepository<ValorInicialDoDonoNaContaPatrimonio, Long> {
 
 	ValorInicialDoDonoNaContaPatrimonio findByDonoAndContaPatrimonio(Dono dono, ContaPatrimonio contaPatrimonio);
